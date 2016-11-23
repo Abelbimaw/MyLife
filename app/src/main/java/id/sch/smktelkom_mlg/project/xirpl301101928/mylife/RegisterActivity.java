54cd8e62ce25a868087e2180cdab7477980f324e
jokerx003 <marabhunta7@gmail.com>
2016-11-23 19:05:09 +0700

    First by Aby

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl301101928/mylife/RegisterActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl301101928/mylife/RegisterActivity.java
@@ -9,13 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-<<<<<<< HEAD
-=======
-/**
- * Created by Mufidatun Nabilah on 11/23/2016.
- */
->>>>>>> origin/master
-
+ 
 public class RegisterActivity extends ActionBarActivity {
     private static final String mypref = "Mypref";
     SharedPreferences sp;
@@ -53,7 +47,3 @@ public class RegisterActivity extends ActionBarActivity {
 
 
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
